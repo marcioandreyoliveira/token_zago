2e397287475483cc330b1dc9a7a039e02ac7f2a5
marcioandreyoliveira <marcioandreyoliveira@gmail.com>
2024-07-17 10:22:30 -0300

    Create token_zago.sol

---
+++ token_zago.sol
@@ -0,0 +1,69 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.2;
+
+interface IERC20 {
+    function totalSupply() external view returns (uint);
+    function balanceOf(address tokenOwner) external view returns (uint balance);
+    function allowance(address tokenOwner, address spender) external view returns (uint remaining);
+
+    function transfer(address to, uint tokens) external returns (bool success);
+    function approve(address spender, uint tokens) external returns (bool success);
+    function transferFrom(address from, address to, uint tokens) external returns (bool success);
+
+    event Transfer(address indexed from, address indexed to, uint tokens);
+    event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
+}
+
+contract DIOZCoin is IERC20 {
+
+    string public constant name = "DIO Zago Coin";
+    string public constant symbol = "DIZ" ;
+    uint constant public decimals = 2;
+    uint256 public _totalSupply = 1000000;
+
+    mapping(address => uint) balances;
+    mapping(address => mapping(address => uint)) allowed;
+
+    constructor() {
+        balances[msg.sender] = _totalSupply;
+    }
+
+    function totalSupply() public override view returns (uint256) {
+        return _totalSupply;
+    }
+
+    function balanceOf(address tokenOwner) public override view returns (uint256) {
+        return balances[tokenOwner];
+    }
+
+    function transfer(address to, uint tokens) public override returns (bool success) {
+        require(tokens <= balances[msg.sender]);
+
+        balances[msg.sender] = balances[msg.sender] - tokens;
+        balances[to] = balances[to] + tokens;
+        emit Transfer(msg.sender, to, tokens);
+        return true;
+    }
+
+    function approve(address spender, uint256 tokens) public override returns (bool success) {
+        allowed[msg.sender][spender] = tokens;
+        emit Approval(msg.sender, spender, tokens);
+        return true;
+    }
+
+    function allowance(address tokenOwner, address spender) public override view returns (uint256) {
+        return allowed[tokenOwner][spender];
+    }
+
+    function transferFrom(address from, address to, uint256 tokens) public override returns (bool) {
+        require(tokens <= balances[msg.sender]);
+        require(tokens <= allowed[from][msg.sender]);
+
+        balances[from] = balances[from] - tokens;
+        allowed[from][msg.sender] = allowed[from][msg.sender] - tokens;
+        balances[to] = balances[to] + tokens;
+        emit Transfer(from, to, tokens);
+        return true;
+    }
+} 
